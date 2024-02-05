d133b7f595b06435e0bc2125c812cdf96c9a3aa3
Cangoo65 <caneraytekin06@hotmail.com>
2024-02-05 20:31:32 +0300

    Proje dosyası ekle.

---
+++ Metotlar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method__Overloading
+{
+    public class Metotlar
+    {
+        public void Topla(int a, int b, out int toplam)
+        {
+            toplam = a + b;
+        }
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public void EkranaYazdir(int veri)
+        {
+            Console.WriteLine(veri);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Method__Overloading;
+
+string sayi = "999";
+bool sonuc = int.TryParse(sayi, out int outSayi); if (sonuc)
+{
+    Console.WriteLine("Başarılı!"); Console.WriteLine(outSayi);
+}
+else
+{
+}
+Console.WriteLine("Başarısız!");
+Metotlar instance = new Metotlar();
+instance.Topla(4, 5, out int toplamSonucu); Console.WriteLine(toplamSonucu);
+
+//Metot Aşırı Yükleme - Overloading
+int ifade = 999;
+instance.EkranaYazdir(Convert.ToString(ifade));
+instance.EkranaYazdir(ifade);
